87697b055759a1d511bd63eee29a2cc0d6061e6e
Santiago Benito <bmsanti@hotmail.com>
2023-02-18 17:02:36 +0100

    Añadida funcionalidad Eliminar Profesor

--- src/app/componentes/profesor-borrar/profesor-borrar.component.ts
+++ src/app/componentes/profesor-borrar/profesor-borrar.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Profesor } from 'src/app/modelos/profesor';
+import { ProfesorService } from 'src/app/servicios/profesor.service';
 
 @Component({
   selector: 'app-profesor-borrar',
   templateUrl: './profesor-borrar.component.html',
-  styleUrls: ['./profesor-borrar.component.css']
+  styleUrls: ['./profesor-borrar.component.css'],
 })
 export class ProfesorBorrarComponent {
+  constructor(
+    private profersorService: ProfesorService,
+    private root: ActivatedRoute
+  ) {}
 
+  titulo: string = 'ELIMINAR PROFESOR';
+  id: number = this.root.snapshot.params['id'];
+  profesor!: Profesor;
+
+  ngOnInit() {
+    this.profesor = this.profersorService.getProfesor(this.id);
+  }
+
+  eliminarProfesor() {
+    this.profersorService.eliminarProfesor(this.id);
+  }
 }
