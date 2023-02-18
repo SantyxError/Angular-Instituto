bd33493fcb1a6e1704f810061c40b7c4ec747896
Santiago Benito <bmsanti@hotmail.com>
2023-02-18 17:30:50 +0100

    Añadido Actualizar Profesor

--- src/app/componentes/profesor-actualizar/profesor-actualizar.component.ts
+++ src/app/componentes/profesor-actualizar/profesor-actualizar.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Profesor } from 'src/app/modelos/profesor';
+import { ProfesorService } from 'src/app/servicios/profesor.service';
 
 @Component({
   selector: 'app-profesor-actualizar',
   templateUrl: './profesor-actualizar.component.html',
-  styleUrls: ['./profesor-actualizar.component.css']
+  styleUrls: ['./profesor-actualizar.component.css'],
 })
 export class ProfesorActualizarComponent {
+  constructor(
+    private profesorService: ProfesorService,
+    private root: ActivatedRoute,
+    private router: Router
+  ) {}
 
+  titulo: string = 'ACTUALIZAR PROFESOR';
+  profesor!: Profesor;
+  id: number = this.root.snapshot.params['id'];
+
+  ngOnInit() {
+    this.profesor = this.profesorService.getProfesor(this.id);
+  }
+
+  actualizarProfesor() {
+    this.profesorService.actualizarProfesor(this.profesor);
+    this.router.navigate(['/profesores/']);
+  }
 }
