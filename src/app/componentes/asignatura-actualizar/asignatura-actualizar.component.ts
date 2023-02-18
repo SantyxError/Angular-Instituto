75fbf1ac1037c4055d1beb61bdb7929d62b4048f
Santiago Benito <bmsanti@hotmail.com>
2023-02-18 19:57:48 +0100

    Agregado Actulizar Asignaturas

--- src/app/componentes/asignatura-actualizar/asignatura-actualizar.component.ts
+++ src/app/componentes/asignatura-actualizar/asignatura-actualizar.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Asignatura } from 'src/app/modelos/asignatura';
+import { AsignaturasService } from 'src/app/servicios/asignaturas.service';
 
 @Component({
   selector: 'app-asignatura-actualizar',
   templateUrl: './asignatura-actualizar.component.html',
-  styleUrls: ['./asignatura-actualizar.component.css']
+  styleUrls: ['./asignatura-actualizar.component.css'],
 })
 export class AsignaturaActualizarComponent {
+  constructor(
+    private asignaturaServicio: AsignaturasService,
+    private root: ActivatedRoute,
+    private router: Router
+  ) {}
 
+  titulo: string = 'ACTUALIZAR ASIGNATURA';
+  asignatura!: Asignatura;
+  id: number = this.root.snapshot.params['id'];
+
+  ngOnInit() {
+    this.asignatura = this.asignaturaServicio.getAsignatura(this.id);
+  }
+
+  actualizarAsignatura() {
+    this.asignaturaServicio.modificarAsignatura(this.asignatura);
+    this.router.navigate(['clases']);
+  }
 }
--- src/app/componentes/asignatura-borrar/asignatura-borrar.component.ts
+++ src/app/componentes/asignatura-borrar/asignatura-borrar.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Asignatura } from 'src/app/modelos/asignatura';
+import { AsignaturasService } from 'src/app/servicios/asignaturas.service';
 
 @Component({
   selector: 'app-asignatura-borrar',
   templateUrl: './asignatura-borrar.component.html',
-  styleUrls: ['./asignatura-borrar.component.css']
+  styleUrls: ['./asignatura-borrar.component.css'],
 })
 export class AsignaturaBorrarComponent {
+  constructor(
+    private asignaturaService: AsignaturasService,
+    private root: ActivatedRoute,
+    private ruta: Router
+  ) {}
 
+  titulo: string = 'ELIMINAR ASIGNATURA';
+  id: number = this.root.snapshot.params['id'];
+  asignatura!: Asignatura;
+
+  ngOnInit() {
+    this.asignatura = this.asignaturaService.getAsignatura(this.id);
+  }
+
+  eliminarAsignatura() {
+    this.asignaturaService.eliminarAsignatura(this.id);
+    this.volver();
+  }
+
+  volver() {
+    this.ruta.navigate(['clases']);
+  }
 }
